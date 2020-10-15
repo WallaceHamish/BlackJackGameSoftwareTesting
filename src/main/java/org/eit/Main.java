1b96cfba1c7369a0a2ec3b2d9f18c5f167cfae60
WallaceHamish <48734639+WallaceHamish@users.noreply.github.com>
2020-10-16 02:43:10 +1300

    Main class for blackjack game. This version contains score initialisation.

--- src/main/java/org/eit/Main.java
+++ src/main/java/org/eit/Main.java
@@ -1,4 +1,28 @@
 package org.eit;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        //** Initial starting score of both the player and computer
+        int computerScore = 0;
+        int userScore = 0;
+
+        while (playAgain()) {
+            computerScore += getRollValue();
+            userScore += getRollValue();
+            //** Break statement implemented inside loop if either the player or computer goes over the score limit
+            if (!isUnderGameLimit(userScore)
+                    || !isUnderGameLimit(computerScore)) {
+                break;
+            }
+            System.out.println("User Score: " + userScore);
+        }
+
+        if (userScore == 0 && computerScore == 0) {
+            System.out.println("Play against the computer to see your score");
+        } else {
+            displayResults(computerScore, userScore);
+        }
+    }
 }
