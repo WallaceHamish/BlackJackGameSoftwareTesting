45f78e54e40cdc69643719b3aff1ff994d0463a8
WallaceHamish <48734639+WallaceHamish@users.noreply.github.com>
2020-10-15 21:14:43 +1300

    Implementation of Dice class with some methods and constructors.

--- src/main/java/org/eit/BlackJack.java
+++
@@ -1,4 +0,0 @@
-package org.eit;
-
-public class BlackJack {
-}
---
+++ src/main/java/org/eit/Dice.java
@@ -0,0 +1,33 @@
+package org.eit;
+
+import java.util.Random;
+
+public class Dice {
+
+    //** Variables **\\
+
+    private final int NumberOfSides = 6;
+    private int value;
+
+    //** Constructor is used to call a roll method is used to set value of the dice
+
+    Dice() {
+        roll();
+    }
+
+    //** Roll method sets the value of the dice to a random number (REQ-2)
+
+    public void roll() {
+        Random randomValue = new Random();
+        value = randomValue.nextInt(NumberOfSides) + 1;
+    }
+
+    //** This method will retrieve the roll value and return the number that represents the roll
+
+    public int getValue() {
+        return value;
+    }
+
+}
+
+
